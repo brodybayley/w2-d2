3f977d565a076f4475b39020d6de0b28bfa434b0
Brody Bayley <brodybayley@gmail.com>
2021-02-09 21:15:19 +0000

    pushing through final changes for hello_timeout

--- hello_timeout.js
+++ hello_timeout.js
@@ -1,6 +1,12 @@
-let delay = () => {
-  console.log('4311o th3r3 w0r1d')
-}
+setTimeout(() => {
+  console.log('4311o')
+  setTimeout(() => {
+    console.log('th3r3')
+    setTimeout(() => {
+      console.log('w0r1d')
+    }, 1000)
+  }, 1000)
+}, 1000)
 
-setTimeout(delay, 3000)
+
 
